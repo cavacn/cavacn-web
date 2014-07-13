6c1b7d3bca6e4e1cd08cdb45a89e2908b3864ae9
cavacn <admin@cavacn.com>
2014-07-13 21:32:24 +0800

    my nodejs and redis

--- onekeyinstall.bat
+++ onekeyinstall.bat
@@ -1,16 +1,21 @@
 cd /home
 mkdir nodejs
 cd nodejs
-yum install gcc -y
+yum install gcc-c++ -y
+yum install tcl -y
 wget http://nodejs.org/dist/v0.10.29/node-v0.10.29.tar.gz
 tar -xvf node-v0.10.29.tar.gz
 cd node-v0.10.29
 ./configure
-make && install
+make
+install
 cd ..
 cd ..
 mkdir redis
 cd redis
-wget http://download.redis.io/releases/redis-2.8.11.tar.gz
-tar -xvf redis-2.8.11.tar.gz
+wget http://download.redis.io/releases/redis-2.8.12.tar.gz
+tar -xvf redis-2.8.12.tar.gz
+cd redis-2.8.12
+make
+make test
 cd ..
